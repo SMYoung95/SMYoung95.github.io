df9d32f1354b7f615333715eb3307e65d108d3df
SMYoung95 <seanyoungat@hotmail.com>
2023-02-26 14:49:22 +0100

    Changes to Portfolio

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+      <nav>
+        <ul>
+          <li><a href="about.html">About Me</a></li>
+          <li><a href="games.html">Games</a></li>
+          <li><a href="#3d-models">3D Models</a></li>
+          <li><a href="#code">Code</a></li>
+          <li><a href="#resume">Resume</a></li>
+          <li><a href="#contact-me">Contact Me</a></li>
+        </ul>
+      </nav>
+  <h1>Sebastian M. Young</h1>
+  <p>Game Developer and 3D Artist</p>
+</header>
